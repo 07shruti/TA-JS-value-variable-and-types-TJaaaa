047fab5e9b8ded353e75aabc29748211acd82b2b
shruti gupta <shruti92@gmail.com>
2024-06-22 23:25:58 +0530

    add values from 1 to 10 using for loop

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,6 +3,10 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
+for ( let i = 0 ; i > 10 ; i = i + 1)
+{
+    console.log (i);
+}
 
 // Using `console.log` log all the evan values from 1 to 10.
 
